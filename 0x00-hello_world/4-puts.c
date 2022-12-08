c67fef8251c6770f716e4189d99892dee83a6e8e
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-08 03:53:05 -0800

    multi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/*
+ *This program prints "programming is like building a multilingual puzzle
+ *
+ */
 int main(void)
 {
-	char puzzle[] = "\"Programming is like building a multilingual puzzle\n";
+	char puzzle[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(puzzle);
 
