6a8a9f3846ad32cfe8831024920303ae4429e84e
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-08 04:31:14 -0800

    new

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * this program shows a size of varios types
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long d;
+	double e;
+
+	printf("Size of a char: %zu byte(s)\n",sizeof(a));
+	printf("Size of an int: %zu byte(s)\n",sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n",sizeof(c));
+	printf("Size of a long long: %zu byte(s)\n",sizeof(d));
+	printf("Size of a float: %zu byte(s)\n",sizeof(e));
+
+	return (0);
+}
