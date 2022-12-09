c722a741a7c84c61c4d9c3a1f538a7a5a073034a
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-09 12:31:53 -0800

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,13 @@
  *
  * This function is used to print a number from 0 to 10 using putchar
  *
- * return: Always zero(success)
+ * return : Always zero(success)
  */
 
 int main(void)
 {
 	int i;
+	
 	for (i = '0'; i <= '9'; ++i)
 	{
 		putchar(i);
