a5ebeb0ef964d9b4fd534e909583db62ac86faa0
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-09 12:36:00 -0800

    numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,6 +4,7 @@
  * main - Entery point
  *
  * This function is used to print a number from 0 to 10 using putchar
+ *I have used for loop
  *
  * return : Always zero(success)
  */
@@ -11,7 +12,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = '0'; i <= '9'; ++i)
 	{
 		putchar(i);
