1abde23294992907b2721cc1736804987f975987
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-09 05:14:34 -0800

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n<0)
+	if (n < 0)
 	{
 		printf("%d", n);
 		printf(" is negative\n");
 	}
-		else if(n>0)
+		else if (n > 0)
 		{
-			printf("%d", n); 
+			printf("%d", n);
 			printf(" is positive\n");
 		}
 		else
