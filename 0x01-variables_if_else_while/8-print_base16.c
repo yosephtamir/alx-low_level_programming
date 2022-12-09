ccfe61db75b2b73e781c642e8201eef2a173a265
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-09 12:27:30 -0800

    hexa decimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,12 +12,14 @@
 int main(void)
 {
 	int i;
+
 	char j;
-	for(i = 48; i < 58; ++i)
+
+	for (i = 48; i < 58; ++i)
 	{
 		putchar(i);
 	}
-	for ( j = 'a'; j < 'g'; ++j)
+	for (j = 'a'; j < 'g'; ++j)
 	{
 		putchar(j);
 	}
