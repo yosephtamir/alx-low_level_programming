ee614e729f81b17623ba1127ead464e9e2e99cba
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-09 12:56:47 -0800

    comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * this function prints a numbers starting from 0-10 or zero
+ * by separating them using comman
+ * in this function i have used for loop
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; ++i)
+	{
+		putchar(i);
+
+		if (i != '9')
+		{
+		putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
