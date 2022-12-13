f18516a406f0d1680eccf97b260c0d600e75d6aa
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-13 12:15:46 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,15 @@
 
 /**
  * add - is used to add two intigers
+ * 
+ * @a: is an intiger
  *
+ * @b: is an intiger value
+ * 
  * Return: a+b
  */
 
-int add(int a, int b)
+int add (int a, int b)
 {
 	return(a + b);
 }
