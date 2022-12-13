b6091d5d7e90d4c2569d68dcd8c50820f80934c3
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-13 05:31:41 -0800

    alphabt

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entery point
@@ -8,14 +8,10 @@
  *  used to print an alphabet in lower case following new line.
  *  and in this project I have also used prototyping since I put a function,
  *  print_alphabet under the main function.
+ *
+ *  Return: Always 0 (Success).
  */
 
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-}
 
 void print_alphabet(void)
 {
@@ -23,7 +19,7 @@ void print_alphabet(void)
 
 	for (i = 'a'; i <= 'z'; ++i)
 	{
-		putchar(i);
-		putchar('\n');
+		_putchar(i);
+		_putchar('\n');
 	}
 }
