ab7685df1fe9b1b42e69ee3ab9cad9bd25c3d1c1
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-13 12:22:05 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * add - is used to add two intigers 
+ * add - is used to add two intigers
+ *
  * @a: is an intiger
  *
  * @b: is an intiger value
@@ -9,7 +10,7 @@
  * Return: a+b
  */
 
-int add (int a, int b)
+int add(int a, int b)
 {
-	return (a+b);
+	return (a + b);
 }
