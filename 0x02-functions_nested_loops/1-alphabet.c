af10fdcaf2ae7c014ca6e002a5a0c4bc2fa98e0f
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-13 05:43:29 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,6 @@
  * main - Entery point
  *
  * description: The project print_alphabet - print alphabet
- * and it has two functions the first one is the main function,
- * which is excuted first.and the second function isthe function,
- * used to print an alphabet in lower case following new line.
- * and in this project I have also used prototyping since I put a function,
- * print_alphabet under the main function.
  *
  * Return: Always 0 (Success).
  */
