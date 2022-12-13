7a636ac6da2bfd230e27aa87f33e53f18ab0f229
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-13 09:27:41 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - shows whether negative, positive or 0
+ *
+ * @n: is an ascii character used for argument
+ *
+ * return: 0,-1, or 1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
