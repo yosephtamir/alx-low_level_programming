304fb94320c34d33c160c8afbac350df00170652
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-13 12:40:59 -0800

    print

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers upto 98
+ *
+ * @n: intigerr
+ *
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+
+}
