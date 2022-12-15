3510596380c88834b48eae09512937a86c5d8b08
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-15 01:02:50 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ *
+ * @c: is ascii character used for argument
+ *
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
