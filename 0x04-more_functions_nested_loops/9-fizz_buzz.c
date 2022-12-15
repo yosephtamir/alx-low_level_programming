7c05e94ce8e021997411bf9bf176eb8a816d2024
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-15 10:34:07 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * REturn: always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		int a, b;
+		a = i % 3;
+		b = i % 5;
+
+		if (a == 0 && b == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (b == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+}
