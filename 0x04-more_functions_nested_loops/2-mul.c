90a81b089cf44290e219e060673ff519875f8ecd
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-15 01:18:58 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies an integer
+ *
+ * Return: a*b
+ *
+ * @a: ascii character from user
+ *
+ * @b: ascii character from user
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
