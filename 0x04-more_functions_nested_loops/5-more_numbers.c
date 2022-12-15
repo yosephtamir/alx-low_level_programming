bd5952b9705a32b68d6aeffc6d443d2d60f617e4
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-15 08:39:06 -0800

    mor numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - used to print 0 to 14 ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int k[] = {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4};
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+				_putchar('1');
+
+			_putchar(k[j] + '0');
+		}
+		_putchar('\n');
+	}
+}
