44f481d99e86ab20f3615bda068046368ea97cbb
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-15 10:03:54 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * @size: argument
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
+
+
