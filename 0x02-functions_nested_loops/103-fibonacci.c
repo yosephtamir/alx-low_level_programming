61e9a0058d306e1e78ece83841d608eb7b65e33b
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2022-12-26 07:50:42 -0800

    even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - is used to add even fibonacci numbers upto 4000000
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long num1, num2, sum, value;
+
+	num1 = 0;
+	num2 = 1;
+	value = 0;
+	sum = num1 + num2;
+
+	while (sum < 4000000)
+	{
+		if (sum % 2 == 0)
+			value = value + sum;
+
+		num1 = num2;
+		num2 = sum;
+		sum = num1 + num2;
+	}
+	printf("%lu\n", value);
+	return (0);
+}
