d5b61fd021130161064e10648247a682caebe69f
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-01-03 12:27:35 -0800

    copy array:

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_string - is used to copy an array fro s to to
+ *
+ * @*s: is array from another function
+ *
+ * @to: is where to copy
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
