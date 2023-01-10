70a4ba5dbcf605e070ca31818d6fe88170818af2
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-01-10 00:33:28 -0800

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - is used to duplcate the char
+ *
+ * @str: is the string to be duplctd.
+ *
+ * Return: NULL or str
+ */
+
+char *_strdup(char *str)
+{
+	char *i;
+	int j = 0, k = 0;
+
+	while (*(str + k) != '\0')
+	{
+		k++;
+	}
+	i = (char *) malloc(k * sizeof(char));
+
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+	else if (str != NULL)
+	{
+		while (j < k)
+		{
+			i[j] = str[j];
+			j++;
+		}
+		return (i);
+	}
+	else
+		return (NULL);
+}
