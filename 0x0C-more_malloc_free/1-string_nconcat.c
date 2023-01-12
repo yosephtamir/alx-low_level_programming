3e380013b955151a002f2a9b53408e726c773993
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-01-12 11:33:47 -0800

    nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - is used to concatinate two strings
+ *
+ * @s1: is the first string to be concatinated
+ *
+ * @s2: is the second string to be concatinated
+ *
+ * @n: is the length of s2
+ * Return: concatinated string S1
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j = 0, k = 0, l = 0, m = 0;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+
+	while (*(s1 + i) != '\0')
+		i++;
+	while (*(s2 + j) != '\0')
+	{
+		j++;
+	}
+
+	if (n >= j)
+		m = i + j + 1;
+	else
+		m = i + n + 1;
+
+
+
+	ptr = malloc(m);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	while (k < i)
+	{
+		ptr[k] = s1[k];
+		k++;
+	}
+	while (l < (m - i))
+	{
+		ptr[k] = s2[l];
+		l++;
+		k++;
+	}
+	return (ptr);
+}
+
