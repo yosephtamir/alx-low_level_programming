cdcfe1bf3b92f908c24b99397fceee1c869702e2
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-01-16 10:26:29 -0800

    duppl

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,33 @@
 #include <stdlib.h>
 
 /**
+ * dup - is used to dupplicate a string
+ * @len: is a character
+ * Return: dup
+ */
+
+char *dup(char *len)
+{
+	unsigned int i = 0, j;
+	char *car;
+
+	while (*(len + i) != '\0')
+		i++;
+
+	car = (char *) malloc(sizeof(char) * i + 1);
+	if (car == NULL)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		car[j] = len[j];
+		j++;
+	}
+	car[j] = '\0';
+	return (car);
+}
+
+/**
  * new_dog - is used to duplicate the struct
  *
  * @name: is name from struct
@@ -14,13 +41,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogdup;
+	char *dogname, *dogowner;
 
 	dogdup = malloc(sizeof(dog_t));
 	if (dogdup == NULL)
 		return (NULL);
-	if (name == NULL || owner == NULL)
+	dogname = dup(name);
+	if (dogname == NULL)
 	{
 		free(dogdup);
+		return (NULL);
+	}
+	dogowner = dup(owner);
+	if (dogowner == NULL)
+	{
+		free(dogname);
+		free(dogowner);
 		return (NULL);
 	}
 	dogdup->name = name;
