27698bb3ed27c9934430c8aeaf964e4c89cae5d7
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-01-16 04:05:19 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - is a struct used to store a name age and owner
+ * @name: is name of the dog
+ * @age: is age of the dog
+ * @owner: is owner of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
