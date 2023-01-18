2b1df48040eae34c6562f4164f14c3eeaf7ad807
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-01-18 10:49:30 -0800

    function pointer task 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - is used to print an array
+ * @array: is an a array to be printed
+ * @size: is size of the array to be printed
+ * @action: is the function used to perform the task
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+	else
+		return;
+}
