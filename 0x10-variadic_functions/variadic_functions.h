24361aad3c4071709bb85986d4bab9e84b87a228
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-01-19 11:45:25 -0800

    fina

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,4 +3,16 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * op - is a struct used for all types
+ * @c: char
+ * @f: function
+ */
+
+typedef struct op
+{
+	char c;
+	void (*f)(va_list);
+} op_t;
+void print_all(const char * const format, ...);
 #endif
