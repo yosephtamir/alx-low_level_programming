728955f9ee09f16e3d8ea5121d74fff49a1f629d
YOSEPHTAMIRAT <yosephtamiratgemechu@gmail.com>
2023-03-16 12:44:40 -0700

    free

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
+
 /**
- * free_dlistint - is used to free mem
- * @head: is to be freed
- * Return: void
+ * free_dlistint - Frees a linked dlistint_t list.
+ * @head: The head of the dlistint_t list.
  */
-void free_dlistint(dlistint_t *head);
+void free_dlistint(dlistint_t *head)
 {
-	dlistint *new;
+	dlistint_t *tmp;
 
 	while (head)
 	{
-		new = head->next;
-		free(head)
-		head = new;
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
