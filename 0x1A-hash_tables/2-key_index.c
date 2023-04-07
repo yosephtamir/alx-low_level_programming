7d95aa937c6e499a28259dd022acca8a765f84b2
Yoseph Tamirat <yosephtamir@gmail.com>
2023-04-07 18:39:36 +0300

    The key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_table.h"
+/**
+ * key_index - is used to give an index of a key
+ * @key: is the key to be assigned
+ * @size: is the size of the list
+ * Return: the index of a key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
