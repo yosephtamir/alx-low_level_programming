65e106f882902764fefee756b57540201a207299
Yoseph Tamirat <yosephtamir@gmail.com>
2023-04-07 18:55:37 +0300

    HEADER FILE

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef _HASH_TABLES_H_
+#define _HASH_TABLES_H_
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -29,4 +32,6 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+#endif
