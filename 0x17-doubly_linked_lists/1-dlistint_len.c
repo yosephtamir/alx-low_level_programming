c73b4dc4178eb6c07dc47ee30c9119923912f97c
Yoseph Tamirat <yosephtamir@gmail.com>
2023-05-18 22:30:32 +0300

    count

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
-
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
- * @h: The head of the dlistint_t list.
- * Return: The number of nodes in the list.
+ * dlistint_len - is used to count number of lists
+ * @h: is the list
+ * dlistint_t: is the struct
+ * Return: node
  */
-size_t print_dlistint(const dlistint_t *h)
+
+size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	size_t node = 0;
 
 	while (h)
 	{
-		nodes++;
-		printf("%d\n", h->n);
+		node++;
+
 		h = h->next;
 	}
-
-	return (nodes);
+	return (node);
 }
