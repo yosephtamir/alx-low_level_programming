3a4dd8a5e3a855197ba2e3b381ffa77e556b0be9
Yoseph Tamirat <yosephtamir@gmail.com>
2023-12-13 23:49:31 +0300

    binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+/**
+ * binary_search - is used to search an integer
+ * @array: the array to be searched in
+ * @size: the length
+ * @value: the integer to be searched
+ * Return: the index or -1 if failed
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i = 0, low = 0, mid = 0, high = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (low <= high)
+	{
+		mid = (high + low) / 2;
+		i = low;
+
+		printf("Searching in array: ");
+
+		for (; i <= high; ++i)
+		{
+			if (i != low)
+				printf(", %d", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+
+		printf("\n");
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
+	}
+
+	return (-1);
+}
