216be5a5f17b316b2f05d6128a1410541d82ae08
Yoseph Tamirat <yosephtamir@gmail.com>
2023-12-13 23:21:18 +0300

    ab

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (value);
+			return (i);
 	}
 	return (-1);
 }
