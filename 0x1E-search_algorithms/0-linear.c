5d3d54aa4ceba334afef8d7fe33efe71f2399939
Yoseph Tamirat <yosephtamir@gmail.com>
2023-12-13 22:32:01 +0300

    real

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (!array || !value)
+	if (array == NULL || value == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
